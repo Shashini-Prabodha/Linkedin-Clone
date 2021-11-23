dff5d59fe3e399ef3d87312a279558d176febb3f
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2021-11-23 17:37:50 +0530

    Create app in react native

---
+++ android/app/src/main/java/com/linkedin_clone/MainActivity.java
@@ -0,0 +1,15 @@
+package com.linkedin_clone;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "linkedin_clone";
+  }
+}
