1f105b7ba0c981f176812a6010cdea680e9cb525
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2021-12-11 14:43:02 +0530

    Cloud notification added....!✨

--- src/view/Notification.js
+++ src/view/Notification.js
@@ -1,8 +1,43 @@
 import React, {Component} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import HeaderSection from '../common/HeaderSection';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import PushNotification from 'react-native-push-notification';
+import Firebase from '@react-native-firebase/app';
+
 
 class Notification extends Component {
+
+    componentDidMount() {
+
+        Firebase.initializeApp(this);
+        PushNotification.configure({
+            onRegister: function (token) {
+                console.log('TOKEN:', token);
+            },
+            onNotification: function (notification) {
+                console.log('NOTIFICATION:', notification);
+
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+            onAction: function (notification) {
+                console.log('ACTION:', notification.action);
+                console.log('NOTIFICATION:', notification);
+            },
+            onRegistrationError: function (err) {
+                console.error(err.message, err);
+            },
+
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true,
+            },
+
+            popInitialNotification: true,
+            requestPermissions: true,
+        });
+    }
 
         render() {
             return (
